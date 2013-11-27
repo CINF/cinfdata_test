7d5d5eefd32b58a81aac556d8a48b5044232f9e5
Kenneth Nielsen <k.nielsen81@gmail.com>
2013-11-27 09:58:23 +0100

    Added another calendar to b307 booking

--- booking/b307.php
+++ booking/b307.php
@@ -7,7 +7,6 @@ echo(html_header());
 
 <div style="float:left;width:550px;padding:10px 10px 0px 10px">
   <b>You have to be looged into the Google account that you use to make reservations with, to see the content of the calendars</b>
-  
   <ul>
     <li><a href="#RDE1-FH1-left">Rotating disc setup 1, on the left in fume hood 1 (RDE1-FH1-left)</a></li>
     <li><a href="#RDE2-FH1-middle">Rotating disc setup 2, in the middle of fume hood 1 (RDE2-FH1-middle)</a></li>
@@ -15,6 +14,7 @@ echo(html_header());
     <li><a href="#RDE4-H2-FH">Rotating disc setup 4, H<sub>2</sub> fume hood (RDE4-H2-FH)</a></li>
     <li><a href="#RDE5-left">Rotating disc setup 5, to the left on lab table (RDE5 left)</a></li>
     <li><a href="#RDE6-right">Rotating disc setup 6, to the right on lab table (RDE6 right)</a></li>
+    <li><a href="#tube-furnace">Tube furnace 307, 2nd floor</a></li>
   </ul>
 </div>
 
@@ -32,7 +32,7 @@ echo(html_header());
 <iframe id="RDE4-H2-FH" src="https://www.google.com/calendar/embed?src=f1ecu01le4vhm1ul6eie5m1nd0%40group.calendar.google.com&ctz=Europe/Copenhagen" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 <iframe id="RDE5-left" src="https://www.google.com/calendar/embed?src=b6r9gp651nghg5k86fdi8fesm0%40group.calendar.google.com&ctz=Europe/Copenhagen" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 <iframe id="RDE6-right" src="https://www.google.com/calendar/embed?src=mj24rfi1oc78te116agva1lm1c%40group.calendar.google.com&ctz=Europe/Copenhagen" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
+<iframe id="tube-furnace" src="https://www.google.com/calendar/embed?src=m5i0cdcnke6blohn61b52vd5gs%40group.calendar.google.com&ctz=Europe/Copenhagen" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
 <?php
 include("../common_functions_v2.php");
