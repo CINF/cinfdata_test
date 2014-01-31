e891644cc3cd05647af3720e9279fdfa9255dbda
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-01-31 12:01:29 +0100

    common: Add gaslevels

--- cinf_common/webcam.php
+++ cinf_common/webcam.php
@@ -1,28 +1,223 @@
 <?php
-system("wget -N -O /var/www/cinfdata/images/cam1.jpg http://rasppi06/camera1/lastsnap.jpg");
-system("wget -N -O /var/www/cinfdata/images/cam2.jpg http://rasppi06/camera2/lastsnap.jpg");
+include("../common_functions.php");
+include("graphsettings.php");
+
+date_default_timezone_set('Europe/Copenhagen');
+
+$db = std_db();
+
+$xscale = date_xscale(); // Get default x-scale
+
+function currentValue($type){
+    $xscale = date_xscale(); // Get default x-scale
+    $db = std_db(); // These two are not inherited from global scope....
+
+    $settings = plot_settings($type,$xscale);
+    $query = $settings['query'];
+    $result = mysql_query($query . " desc limit 1",$db);
+    $row = mysql_fetch_array($result);
+    $current_value = $row;
+    return($current_value);
+}
+
+#Channel 01
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) FROM gasmonitor_ch01 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch01_time = $row[0];
+$ch01_value = $row[1];
+
+#Channel 02
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) +4.2  FROM gasmonitor_ch02 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch02_time = $row[0];
+$ch02_value = $row[1];
+
+#Channel 03
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) -5 FROM gasmonitor_ch03 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch03_time = $row[0];
+$ch03_value = $row[1];
+
+#Channel 04
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) -9.5 FROM gasmonitor_ch04 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch04_time = $row[0];
+$ch04_value = $row[1];
+
+#Channel 09
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) +6 FROM gasmonitor_ch09 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch09_time = $row[0];
+$ch09_value = $row[1];
+
+#Channel 11
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) +5 FROM gasmonitor_ch11 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch11_time = $row[0];
+$ch11_value = $row[1];
+
+#Channel 13
+$query = "SELECT unix_timestamp(time), (6.25*value - 25) +3 FROM gasmonitor_ch13 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch13_time = $row[0];
+$ch13_value = $row[1];
+
+#Channel 05
+$query = "SELECT unix_timestamp(time), 15.625*(value - 4) +0.5 FROM gasmonitor_ch05 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch05_time = $row[0];
+$ch05_value = $row[1];
+
+#Channel 06
+$query = "SELECT unix_timestamp(time), 15.625*(value - 4) +1.6 FROM gasmonitor_ch06 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch06_time = $row[0];
+$ch06_value = $row[1];
+
+#Channel 07
+$query = "SELECT unix_timestamp(time), 15.625*(value - 4) +0.8 FROM gasmonitor_ch07 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch07_time = $row[0];
+$ch07_value = $row[1];
+
+#Channel 08
+$query = "SELECT unix_timestamp(time), 15.625*(value - 4) +1.1 FROM gasmonitor_ch08 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch08_time = $row[0];
+$ch08_value = $row[1];
+
+#Channel 10
+$query = "SELECT unix_timestamp(time), 15.625*(value - 4) +0.8 FROM gasmonitor_ch10 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch10_time = $row[0];
+$ch10_value = $row[1];
+
+#Channel 12
+$query = "SELECT unix_timestamp(time), 15.625*(value - 4) -1.1 FROM gasmonitor_ch12 order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$ch12_time = $row[0];
+$ch12_value = $row[1];
+
 ?>
+
+
+
 <html>
 <head>
 <title>CINF data logging</title>
 <style title="css" type="text/css">@import "../style.css";</style>
+
+<style>
+.down {
+background: #ff0000;
+width: 15px;
+height: 15px;
+  border-radius: 50%;
+}
+</style>
+
+<style>
+.up {
+background: #00ff00;
+width: 15px;
+height: 15px;
+  border-radius: 50%;
+  }
+</style>
+
 </head>
 <body>
 
-<h1 id="commonstatus">CINF webcams</h1>
+
+<h1 id="commonstatus">Gasmonitor, B312</h1>
 
 <img src="../cinf_logo_web.png" id="logo">
 
+<h2 id="commonstatustitle">CH<sub>4</sub></h2>
 <table cellpadding="5">
  <tr align="center">
   <td>
-   <img src="https://cinfdata.fysik.dtu.dk/images/cam1.jpg">
+   <img src="plot.php?type=gas_monitor_ch4&small_plot=1&ymin=-1&ymax=20">
   </td>
   <td>
-   <img src="https://cinfdata.fysik.dtu.dk/images/cam2.jpg">
+   <img src="plot.php?type=gas_monitor_ch4_2&small_plot=1&ymin=-1&ymax=20">
+  </td>
+  <td>
+
+
+<!--
+  <table border=1 padding=5>
+<?php
+  for($rows=0;$rows<8;$rows++){
+    echo("<tr>");
+    for($i=0;$i<13;$i++){
+      echo("<td class=\"down\">&nbsp;</td>");
+    }
+    echo("</tr>");
+  }
+?>
+  </table>
+-->
+  &nbsp;
+  </td>
+ </tr>
+ <tr>
+  <td>
+  <font color="0000FF">01, Pumperum: <?php echo round($ch01_value,1)?> %lel <!-- recorded @ <?php echo date("D M j H:i Y",$ch01_time)?>--></font>  <br>
+  <font color="FF0000">02, Hal (vest): <?php echo round($ch02_value,1)?> %lel</font><br>
+  <font color="00FF00">03, Hal (&oslash;st): <?php echo round($ch03_value,1)?> %lel</font><br>
+  <font color="000000">04, Kemilab: <?php echo round($ch04_value,1)?> %lel</font><br>
+  </td>
+  <td>
+  <font color="0000FF">09, Hall 074: Outer wall, down: <?php echo round($ch09_value,1)?> %lel</font><br>
+  <font color="FF0000">11, Hall 074: Outer wall, up: <?php echo round($ch11_value,1)?> %lel</font><br>
+  <font color="00FF00">13, Hall 074: Ventilation system: <?php echo round($ch13_value,1)?> %lel</font><br>
+  </td>
+  <td>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <h2 id="commonstatustitle">CO</h1>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <img src="plot.php?type=gas_monitor_CO&small_plot=1&ymin=-3&ymax=30">
+  </td>
+  <td>
+   <img src="plot.php?type=gas_monitor_CO_2&small_plot=1&ymin=-3&ymax=30">
+  </td>
+  <td>
+  &nbsp;
+  </td>
+ </tr>
+ <tr>
+  <td>
+  <font color="0000FF">05, Pumperum: <?php echo round($ch05_value,1)?> ppm</font><br>
+  <font color="FF0000">06, Hal (vest): <?php echo round($ch06_value,1)?> ppm</font><br>
+  <font color="00FF00">07, Hal (&oslash;st): <?php echo round($ch07_value,1)?> ppm</font><br>
+  </td>
+  <td>
+  <font color="0000FF">08, Kemilab: <?php echo round($ch08_value,1)?> ppm</font><br>
+  <font color="FF0000">10, Hall 074: Outer wall, down: <?php echo round($ch10_value,1)?> ppm</font><br>
+  <font color="00FF00">12, Hall 074: Inner wall down:<?php echo round($ch12_value,1)?> ppm</font><br>
+
   </td>
  </tr>
 </table>
-Captured by a Raspberry Pi :-)
 </body>
 </html>
---
+++ cinf_common/webcam_dev.php
@@ -0,0 +1,125 @@
+<?php
+include("../common_functions.php");
+include("graphsettings.php");
+
+date_default_timezone_set('Europe/Copenhagen');
+
+$db = std_db();
+
+$xscale = date_xscale(); // Get default x-scale
+
+function currentValue($type){
+    $xscale = date_xscale(); // Get default x-scale
+    $db = std_db(); // These two are not inherited from global scope....
+
+    $settings = plot_settings($type,$xscale);
+    $query = $settings['query'];
+    $result = mysql_query($query . " desc limit 1",$db);
+    $row = mysql_fetch_array($result);
+    $current_value = $row;
+    return($current_value);
+}
+
+# GET MICROREACTOR TEMPERATURE
+$query = "SELECT unix_timestamp(time), temperature FROM temperature_microreactor order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$microreactor_temperature_time = $row[0];
+$microreactor_temperature = $row[1];
+
+# GET MICROREACTORNG TEMPERATURE
+$query = "SELECT unix_timestamp(time), value FROM dateplots_microreactorNG where type = 'T_sample' order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$microreactorNG_temperature_time = $row[0];
+$microreactorNG_temperature = $row[1];
+
+# GET MICROREACTORNG CONTAINMENT PRESSURE
+$query = "SELECT unix_timestamp(time), value FROM dateplots_microreactorNG where type = 'P_containment' order by time desc limit 1";
+$result  = mysql_query($query,$db);  
+$row = mysql_fetch_array($result);
+$microreactorNG_pressure_time = $row[0];
+$microreactorNG_pressure = $row[1];
+
+# GET MICROREACTORNG MASSSPECTRUM ID
+$query = "SELECT id FROM measurements_microreactorNG where type = 4 order by time desc limit 1";
+$microreactorNG_id = single_sql_value($db,$query,0);
+
+# GET MICROREACTOR MASSSPECTRUM ID
+$query = "SELECT id FROM measurements_microreactor where type = 4 order by time desc limit 1";
+$microreactor_id = single_sql_value($db,$query,0);
+?>
+
+
+
+<html>
+<head>
+<title>CINF data logging</title>
+<style title="css" type="text/css">@import "../style.css";</style>
+</head>
+<body>
+
+
+<h1 id="commonstatus">Status of CINF microreactors</h1>
+
+<img src="../cinf_logo_web.png" id="logo">
+
+<h2 id="commonstatustitle">Microreactor</h2>
+<table cellpadding="5">
+ <tr align="center">
+  <td>
+   <img src="plot.php?type=microreactor_overview&small_plot=1"> 
+f </td>
+  <td>
+   <img src="plot.php?type=microreactor_pressure_overview&small_plot=1">
+  </td>
+  <td>
+   <img src="https://cinfdata.fysik.dtu.dk/microreactor/plot.php?type=massspectrum&idlist[]=<?php echo($microreactor_id)?>&offsetlist[<?php echo($microreactor_id)?>]=0" width="325px" height="200px">
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <?php $res = currentValue("microreactor_overview");?>
+   <font color="0000FF"><?php echo science_number($res[1])?> mbar recorded @ <?php echo date("D M j H:i Y",$res[0])?></font><br>
+   <font color="FF0000"><?php echo round($microreactor_temperature,2)?> C recorded @ <?php echo date("D M j H:i Y",$microreactor_temperature_time)?></font>
+  </td>
+  <td>
+   <?php $res = currentValue("microreactor_pressure_overview");?>
+   <font color="0000FF"><?php echo science_number($res[1])?> mbar recorded @ <?php echo date("D M j H:i Y",$res[0])?></font>
+  </td>
+  <td>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <h2 id="commonstatustitle">MicroreactorNG</h1>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <img src="plot.php?type=microreactorNG_overview&small_plot=1">
+  </td>
+  <td>
+   <img src="plot.php?type=microreactorNG_pressure_overview&small_plot=1">
+  </td>
+  <td>
+   <img src="https://cinfdata.fysik.dtu.dk/microreactorNG/plot.php?type=massspectrum&idlist[]=<?php echo($microreactorNG_id)?>&offsetlist[<?php echo($microreactorNG_id)?>]=0" width="325px" height="200px">
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <?php $res = currentValue("microreactorNG_overview");?>
+   <font color="0000FF"><?php echo science_number($res[1])?> mbar recorded @ <?php echo date("D M j H:i Y",$res[0])?></font><br>
+   <font color="FF0000"><?php echo round($microreactorNG_temperature,2)?> C recorded @ <?php echo date("D M j H:i Y",$microreactorNG_temperature_time)?></font>
+  </td>
+  <td>
+   <?php $res = currentValue("microreactorNG_pressure_overview");?>
+   <font color="0000FF"><?php echo science_number($res[1])?> mbar recorded @ <?php echo date("D M j H:i Y",$res[0])?></font><br>
+   <font color="FF0000"><?php echo science_number($microreactorNG_pressure)?> mbar recorded @ <?php echo date("D M j H:i Y",$microreactorNG_pressure_time)?></font>
+  </td>
+  <td>
+  </td>
+ </tr>
+</table>
+</body>
+</html>
