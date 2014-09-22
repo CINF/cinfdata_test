7ad970959af005add7e3eff739f0077c5d013fc2
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-09-22 15:03:12 +0200

    Added todo.php which redirects to the floor managers todo on trello

---
+++ todo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=http://trello.com/b/SQ9tbyrA/floormanagers-to-do">
+        <script type="text/javascript">
+            window.location.href = "http://trello.com/b/SQ9tbyrA/floormanagers-to-do"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        If you are not redirected automatically, follow the <a href='http://trello.com/b/SQ9tbyrA/floormanagers-to-do'>link to example</a>
+    </body>
+</html>
