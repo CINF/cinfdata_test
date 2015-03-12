a3fa18fdbe3c848cadab0ed1ecf6fb051f46abd2
Kenneth Nielsen <k.nielsen81@gmail.com>
2015-03-12 13:12:34 +0100

    tof: Update to live plots interval

--- cinf_common/host_checker.php
+++ cinf_common/host_checker.php
@@ -1,6 +1,9 @@
 <html>
 <head>
 
+<script src="sortable-0.5.0/js/sortable.min.js"></script>
+<link rel="stylesheet" href="sortable-0.5.0/css/sortable-theme-finder.css" />
+
 <style>
 .down {
 background: #ff0000;
@@ -29,21 +32,35 @@ border-radius: 50%;
 $file = file_get_contents('http://robertj/hosts.php');
 $output = explode("\n",$file);
 
-echo("<table border=1 padding=5>");
-echo("<tr><th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Description</th><th>OS</th></tr>");
+#echo("<table border=1 padding=5>");
+#echo("<tr><th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th></tr>");
+
+echo("<table class=\"sortable-theme-finder\" data-sortable>\n");
+echo("<thead>");
+echo("<th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th><th>Git</th>");
+echo("</thead><tbody>");
+
 for ($i=0;$i<sizeof($output)-2;$i++){
   $row = explode(';',$output[$i]);
   echo("<tr>");
-  $color = ($row[1] == 0) ? "\"down\"" : "\"up\"";
-  echo("<td class=" . $color . ">&nbsp;</td>");
+  $color = ($row[1] == 0) ? "\"#FF0000\"" : "\"#00FF00\"";
+  $value = ($row[1] == 0) ? "0" : "1";
+
+  #echo("<td class=" . $color . ">&nbsp;</td>");
+  echo("<td data-value=\"" . $value . "\"><font color=" . $color . ">&#8226;</font></td>");
   echo("<td>" . $row[0] . "</td>");
-  $uptime = ($row[1] == 0) ? "<b>Host is down</b>" : $row[2];
-  echo("<td>" . $uptime . "</td>");
-  echo("<td>" . $row[3] . "</td>");
+  if ($row[1] == 0){
+    echo("<td colspan=2><b>Host is down</b></td>");
+  }else{
+       echo("<td>" . $row[2] . "</td><td>" . $row[3] . "</td>");
+  }
   echo("<td>" . $row[4] . "</td>");
+  echo("<td>" . $row[5] . "</td>");
+  echo("<td>" . $row[6] . "</td>");
+  echo("<td>" . $row[7] . "</td>");
   echo("</tr>\n");
 }
-echo("</table>");
+echo("</tbody></table>");
 ?>
 </body>
 </html>
