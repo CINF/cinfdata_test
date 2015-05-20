19bd39f008bca5088b6a070dc421e604463265e6
Robert Jensen <jensen.robert@gmail.com>
2015-05-20 10:21:46 +0200

    Added temperature and python version to host checker

--- cinf_common/host_checker.php
+++ cinf_common/host_checker.php
@@ -33,22 +33,28 @@ $file = file_get_contents('http://robertj/hosts.php');
 $output = explode("\n",$file);
 
 #echo("<table border=1 padding=5>");
-#echo("<tr><th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th></tr>");
+#echo("<tr><th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th><th>Temp.</th><th>Model</th></tr>");
 
 echo("<table class=\"sortable-theme-finder\" data-sortable>\n");
 echo("<thead>");
-echo("<th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th><th>Git</th>");
+echo("<th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th><th>Git</th><th>Temp.</th><th>Python</th><th>Model</th>");
 echo("</thead><tbody>");
 
 for ($i=0;$i<sizeof($output)-2;$i++){
-  $row = explode(';',$output[$i]);
+  $row = explode('|',$output[$i]);
   echo("<tr>");
   $color = ($row[1] == 0) ? "\"#FF0000\"" : "\"#00FF00\"";
   $value = ($row[1] == 0) ? "0" : "1";
 
   #echo("<td class=" . $color . ">&nbsp;</td>");
   echo("<td data-value=\"" . $value . "\"><font color=" . $color . ">&#8226;</font></td>");
-  echo("<td>" . $row[0] . "</td>");
+  if (substr($row[0], 0, 6) == 'rasppi'){
+    $sortval = (substr($row[0], 6));
+  }
+  else{
+    $sortval = 1000;
+  }
+  echo("<td data-value=\"" . $sortval . "\">" . $row[0] . "</td>");
   if ($row[1] == 0){
     echo("<td colspan=2><b>Host is down</b></td>");
   }else{
@@ -58,6 +64,9 @@ for ($i=0;$i<sizeof($output)-2;$i++){
   echo("<td>" . $row[5] . "</td>");
   echo("<td>" . $row[6] . "</td>");
   echo("<td>" . $row[7] . "</td>");
+  echo("<td>" . $row[8] . "</td>");
+  echo("<td>" . $row[10] . "</td>");
+  echo("<td>" . $row[9] . "</td>");
   echo("</tr>\n");
 }
 echo("</tbody></table>");
