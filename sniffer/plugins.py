ccdb9ec374a17841484f4a80c809f52d890eb15d
Robert Jensen <robert.jensen@fysik.dtu.dk>
2016-04-19 07:39:51 +0200

    Updated live-plots and plugins for a number of setups

--- sniffer/plugins.py
+++ sniffer/plugins.py
@@ -39,3 +39,46 @@ class MassSpectraOffset(object):
         self.label_additions['y_right_label_addition'] = message
 
         return self.label_additions
+
+class MassTimeOffset(object):
+    """Plugin that will offset mass spectra by -1.1 times the minimum y-value
+    found in all of the spectra to prevent errors when plotting on log scale
+    """
+
+    def __init__(self, settings, plot_options, ggs=None):
+        self.settings = settings
+        self.label_additions = {
+            'xlabel_addition': '',
+            'y_left_label_addition': '',
+            'y_right_label_addition': '',
+        }
+
+    def run(self, left, right):
+        """The main run method"""
+        # Find minimum value
+        min_ = 1
+        for dat in left + right:
+            local_min = 1
+            mass_label = dat['meta']['mass_label']
+            if mass_label[0] == 'M' and mass_label[1].isdigit():
+                min_ = min(numpy.min(dat['data'][:, 1]), min_)
+
+        if min_ <= 0:
+            for dat in left + right:
+                mass_label = dat['meta']['mass_label']
+                if mass_label[0] == 'M' and mass_label[1].isdigit():
+                    min_ = min(numpy.min(dat['data'][:, 1]), min_)
+                    # Shift the data
+                    shift = min_ * -1.1
+                    dat['data'][:, 1] += shift
+                    # Print the offset and change the axis labels
+            print 'All masses offset by: {0}'.format(shift)
+            message = 'Data has been offset'
+        else:
+            print mass_label + ': No offset was necessary.'
+            message = 'No offset applied'
+
+        self.label_additions['y_left_label_addition'] = message
+        self.label_additions['y_right_label_addition'] = message
+
+        return self.label_additions
--- tof/plugins.py
+++ tof/plugins.py
@@ -73,7 +73,7 @@ class Massscale(object):
                     tm.Voltages['R2'] = dat['meta']['tof_R2_voltage']
                     coeff = tof_helpers.extrapolate()
                     print coeff
-                    dat['data'][:, 0] = ((dat['data'][:, 0]-0.31)/coeff[0])**(1/coeff[1])
+                    dat['data'][:, 0] = ((dat['data'][:, 0] + 0.1)/coeff[0])**(1/coeff[1])
             else:
                 p1_0 = dat['meta']['tof_p1_0']
                 p1_1 = dat['meta']['tof_p1_1']
@@ -192,7 +192,71 @@ class Intensitymap_wide(object):
                 start_index = 100 # Cut off the very first part of the spectrum
                 end_index = dat['data'].shape[0]
             old_size = end_index - start_index
-            new_size = 650
+            new_size = 1900
+            ratio = (old_size / new_size)+1
+            print 'Ratio: ' + str(ratio)
+
+            new = numpy.zeros((new_size, 2))
+            for i in range(0, new_size):
+                values = dat['data'][start_index+(ratio * i):start_index+(ratio * (i + 1)), 0]
+                x_val = numpy.mean(values)
+                values = dat['data'][start_index+(ratio * i):start_index+(ratio * (i + 1)), 1]
+                try:
+                    y_val = max(values)
+                except ValueError:
+                    y_val = 0
+                #print('Mass: ' + str(x_val) + ', value: ' + str(y_val))
+                new[i,0] = x_val
+                new[i,1] = y_val
+            dat['data'] = new
+
+        self.label_additions['xlabel_addition'] = \
+            'Converted to mass'
+        return self.label_additions
+
+class Intensitymap_ultrawide(object):
+    def __init__(self, settings, plot_options, ggs=None):
+        self.settings = settings
+        self.range = plot_options['xscale_bounding']
+        if self.range is None:
+            self.range = [0,0]
+        self.label_additions = {
+            'xlabel_addition': '',
+            'y_left_label_addition': '',
+            'y_right_label_addition': '',
+        }
+
+    def run(self, left, right):
+        for dat in left + right:
+            t = time.time()
+            if dat['meta']['tof_p1_0'] is None:
+                if dat['meta']['tof_R1_voltage'] == 0:
+                    dat['data'][:, 1] = 0
+                else:
+                    tm.Voltages = {} # Contains all electrical values of the maching
+                    tm.Voltages['pulse'] = dat['meta']['tof_pulse_voltage']
+                    tm.Voltages['liner'] = dat['meta']['tof_liner_voltage']
+                    tm.Voltages['R1'] = dat['meta']['tof_R1_voltage']
+                    tm.Voltages['R2'] = dat['meta']['tof_R2_voltage']
+                    coeff = tof_helpers.extrapolate()
+                    dat['data'][:, 0] = ((dat['data'][:, 0]+0.1)/coeff[0])**(1/coeff[1])
+            else:
+                p1_0 = dat['meta']['tof_p1_0']
+                p1_1 = dat['meta']['tof_p1_1']
+                p1_2 = dat['meta']['tof_p1_2']
+                dat['data'][:, 0] = p1_1*pow(dat['data'][:, 0]-p1_0, p1_2)
+                print 'Found in db lookup!'
+            print time.time() - t
+
+
+            if self.range[1] > self.range[0]:
+                start_index = numpy.argmax(dat['data'][:,0]>self.range[0])
+                end_index =  numpy.argmax(dat['data'][:,0]>self.range[1])
+            else:
+                start_index = 100 # Cut off the very first part of the spectrum
+                end_index = dat['data'].shape[0]
+            old_size = end_index - start_index
+            new_size = 30000
             ratio = (old_size / new_size)+1
             print 'Ratio: ' + str(ratio)
 
